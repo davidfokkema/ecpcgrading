43d7e271ef2314345cea7f7b4898185ab8af0eda
David Fokkema <davidfokkema@icloud.com>
2023-10-27 20:58:20 +0200

    Single source for version number

--- src/ecpcgrading/__init__.py
+++ src/ecpcgrading/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.2.0"
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("ecpcgrading")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = None
